b980e53235b237806c2aed3ca7b2021ca1e843f0
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-02-01 16:12:57 +0530

    leetcode

---
+++ KDiffPairInArray.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c)  2023 by Sanidhya Varshney
+ */
+import java.util.*;
+public class KDiffPairInArray {
+        public int findPairs(int[] nums, int k) {
+            Map<Integer,Integer> map=new HashMap<>();
+            for(int num:nums){
+                map.put(num,map.getOrDefault(num,0)+1);
+            }
+
+            int result=0;
+
+            for(int key:map.keySet()){
+                if(k>0 && map.containsKey(key+k)|| k==0 && map.get(key)>1 ) result++;
+            }
+            return result;
+        }
+}
